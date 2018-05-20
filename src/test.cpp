37eff7a3c45c450be901e78288725c0e477baed1
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2018-05-20 15:02:56 +0200

    First rough version of an allocator.

--- src/test.cpp
+++ src/test.cpp
@@ -24,5 +24,64 @@ The fact that you are presently reading this means that you have had knowledge o
 its terms.*/
 
 #include <iostream>
+#include <memory>
+#include <vector>
 
-int main() { std::cout << "Hello world!\n"; }
+using namespace std;
+
+struct Component {
+    virtual ~Component() = default;
+};
+
+struct AllocatedMemory {
+    virtual ~AllocatedMemory() = default;
+};
+
+struct Lifetime {  // deallocates components when destroyed, moveable but non-copyable
+    unique_ptr<AllocatedMemory> data;
+};
+
+template <class C>
+struct ConcreteAllocatedMemory : public AllocatedMemory {
+    ConcreteAllocatedMemory(int nb) { data.reserve(nb); }
+    vector<C> data;
+};
+
+struct AllocatedComponents {
+    AllocatedComponents(int nb) { components.reserve(nb); }
+    Lifetime lifetime;
+    vector<Component*> components;
+    template <class C>
+    C& at(int i) {
+        return dynamic_cast<C&>(*components.at(i));
+    }
+    template <class C>
+    vector<C>& vec() {
+        return dynamic_cast<ConcreteAllocatedMemory<C>&>(*lifetime.data.get()).data;
+    }
+};
+
+template <class C, class... Args>
+AllocatedComponents allocate(int nb, Args... args) {
+    AllocatedComponents alloc(nb);
+    unique_ptr<ConcreteAllocatedMemory<C>> data(new ConcreteAllocatedMemory<C>(nb));
+    for (int i = 0; i < nb; ++i) {
+        data->data.emplace_back(args...);
+        alloc.components.push_back(dynamic_cast<Component*>(&data->data.at(i)));
+    }
+    alloc.lifetime = {unique_ptr<AllocatedMemory>(dynamic_cast<AllocatedMemory*>(data.release()))};
+    return alloc;
+}
+
+struct MyCompo : public Component {
+    int data;
+    MyCompo(int data) : data(data) {}
+};
+
+int main() {
+    auto alloc = allocate<MyCompo>(5, 17);
+    alloc.at<MyCompo>(3).data = 19;
+    for (auto&& e : alloc.vec<MyCompo>()) {
+        cout << e.data << endl;
+    }
+}
